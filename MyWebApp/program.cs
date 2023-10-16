dee833eda962c8ba2715fc4d2e7a0abf3b46688c
vaishakh <vaisakh.k@gds.ey.com>
2023-10-16 15:05:09 +0530

    updated version v7

--- MyWebApp/program.cs
+++ MyWebApp/program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "WELOCOME TO MY APP V6!");
+app.MapGet("/", () => "WELOCOME TO MY APP V7!");
 
 app.Run();

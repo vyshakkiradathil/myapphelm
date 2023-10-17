b29968e5cb9dab7c0ff9dad0d9b4010fcf0d1ec6
vaishakh <vaisakh.k@gds.ey.com>
2023-10-17 10:34:31 +0530

    v9

--- MyWebApp/program.cs
+++ MyWebApp/program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "WELOCOME TO MY APP V8!");
+app.MapGet("/", () => "WELOCOME TO MY APP V9!");
 
 app.Run();

9f42e1aba1401c1ea30396fefc6acc6ffb022bbf
vaishakh <vaishakhvaisu@gmail.com>
2023-10-30 13:43:57 +0530

    v25

--- MyWebApp/program.cs
+++ MyWebApp/program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "WELOCOME TO MY APP V24!");
+app.MapGet("/", () => "WELOCOME TO MY APP V25!");
 
 app.Run();

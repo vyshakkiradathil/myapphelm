7d7e2cf254a9f83f046f5a524d926d31ce0386a6
vaishakh k <38469664+vyshakkiradathil@users.noreply.github.com>
2024-02-25 20:22:16 +0530

    Update program.cs

--- MyWebApp/program.cs
+++ MyWebApp/program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "WELOCOME TO MY APP V25!");
+app.MapGet("/", () => "WELOCOME TO MY APP V26!");
 
 app.Run();
